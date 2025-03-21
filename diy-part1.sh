b6d2bfd2f1b8b3b527f6865cb0dd34ac1ff0504f
aman <524611187@qq.com>
2025-03-21 16:26:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,9 @@
 
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+
+
+# 更新golang
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/tree/openwrt-23.05/lang/golang
+popd
