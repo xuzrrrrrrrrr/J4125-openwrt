99ce36eabae041f46969f7cc2fd81f769df4ff20
aman <524611187@qq.com>
2025-03-22 14:59:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@
 
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
